7bdf87658ad52709cfbf77e2522f5f66b390ef4b
Greg Dardis <dardis.greg@gmail.com>
2018-07-14 15:11:01 -0700

    created enums and properties for LiftingEquipment and MuscleGroups

---
+++ FitnessAPI/FitnessAPI/Models/Exercise.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessAPI.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public MuscleGroup MainMuscleGroup { get; set; }
+        public MuscleGroup[] MuscleGroups { get; set; }
+        public LiftingEquipment[] Equipment { get; set; }
+    }
+}
---
+++ FitnessAPI/FitnessAPI/Models/MuscleGroup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessAPI.Models
+{
+    public enum MuscleGroup
+    {
+        Abs,
+        Back,
+        Biceps,
+        Calves,
+        Chest,
+        Forearms,
+        Glutes,
+        Hamstrings,
+        LowerBack,
+        Quads,
+        Shoulders,
+        Traps,
+        Triceps
+    }
+}
