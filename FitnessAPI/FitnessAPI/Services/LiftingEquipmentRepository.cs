25fe738b6eb3cfcdea4553253809d3e4b4a28b80
Greg Dardis <dardis.greg@gmail.com>
2018-07-25 11:05:28 -0700

    created LiftingEquipmentController and repository, made get all lifting equipment endpoint

---
+++ FitnessAPI/FitnessAPI/Services/LiftingEquipmentRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FitnessAPI.Data;
+using FitnessAPI.Entities;
+
+namespace FitnessAPI.Services
+{
+    public class LiftingEquipmentRepository : ILiftingEquipmentRepository
+    {
+        private FitnessApiDbContext _context;
+
+        public LiftingEquipmentRepository(FitnessApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<LiftingEquipment> GetLiftingEquipment()
+        {
+            return _context.LiftingEquipment.ToList();
+        }
+    }
+}
